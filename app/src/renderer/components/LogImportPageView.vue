1b0d261db882f80cabfe730bcc1f07ec6701c8b0
leozhou <492753662@qq.com>
2017-03-13 12:39:51 +0800

    import log download data

--- app/src/renderer/components/LogImportPageView.vue
+++ app/src/renderer/components/LogImportPageView.vue
@@ -1,19 +1,18 @@
 <template>
 <div>
-    <el-steps :space="100" :active="active" finish-status="success">
-        <el-step title="步骤 1"></el-step>
+    <el-steps :space="150" :active="active" finish-status="success">
+        <el-step title="日志列表"></el-step>
         <el-step title="步骤 2"></el-step>
         <el-step title="步骤 3"></el-step>
     </el-steps>
 
     <div class="step1" v-if="active === 0">
         <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="活动形式1">
-                <el-input type="textarea" v-model="form.desc"></el-input>
+            <el-form-item label="日志列表Json">
+                <el-input type="textarea" v-model="form.logListJson"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                <el-button>取消</el-button>
+                <el-button type="primary" @click="next">开始解析</el-button>
             </el-form-item>
         </el-form>
     </div>
@@ -42,7 +41,7 @@
         </el-form>
     </div>
 
-    <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+    <!-- <el-button style="margin-top: 12px;" @click="next">下一步</el-button> -->
 
 </div>
 </template>
@@ -53,13 +52,7 @@ export default {
         return {
             active: 0,
             form: {
-                name: '',
-                region: '',
-                date1: '',
-                date2: '',
-                delivery: false,
-                type: [],
-                resource: '',
+                logListJson: '',
                 desc: ''
             }
         };
