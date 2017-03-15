ec99c9c3bd841b1b5717f3f6409e5e56f6fc8351
leozhou <492753662@qq.com>
2017-03-16 02:15:03 +0800

    init step

--- app/src/renderer/components/LogImportPageView.vue
+++ app/src/renderer/components/LogImportPageView.vue
@@ -2,14 +2,14 @@
 <div>
     <el-steps :space="150" :active="active" finish-status="success">
         <el-step title="日志列表"></el-step>
-        <el-step title="步骤 2"></el-step>
-        <el-step title="步骤 3"></el-step>
+        <el-step title="下载并解压"></el-step>
+        <el-step title="录入数据"></el-step>
     </el-steps>
 
-    <div class="step1" v-if="active === 0">
+    <div class="step1 step-contain" v-if="active === 0">
         <el-form ref="form" :model="form" label-width="80px">
             <el-form-item label="日志列表Json">
-                <el-input type="textarea" v-model="form.logListJson"></el-input>
+                <el-input class="json-input" type="textarea" v-model="form.logListJson"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="next">开始解析</el-button>
@@ -17,32 +17,31 @@
         </el-form>
     </div>
 
-    <div class="step2" v-if="active === 1">
+    <div class="step2 step-contain" v-if="active === 1">
         <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="活动形式2">
+            <el-form-item label="下载并解压">
                 <el-input type="textarea" v-model="form.desc"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                <el-button>取消</el-button>
+                <el-button type="primary" @click="next">开始下载</el-button>
             </el-form-item>
         </el-form>
     </div>
 
-    <div class="step3" v-if="active === 2">
+    <div class="step3 step-contain" v-if="active === 2">
         <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="活动形式3">
+            <el-form-item label="录入数据">
                 <el-input type="textarea" v-model="form.desc"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                <el-button>取消</el-button>
+                <el-button type="primary" @click="next">开始录入</el-button>
             </el-form-item>
         </el-form>
     </div>
 
-    <!-- <el-button style="margin-top: 12px;" @click="next">下一步</el-button> -->
-
+    <div class="step3 step-contain" v-if="active === 3">
+      <el-button style="margin-top: 12px;" @click="next">完成</el-button>
+    </div>
 </div>
 </template>
 
@@ -60,7 +59,12 @@ export default {
 
     methods: {
         next() {
-            if (this.active++ > 2) this.active = 0;
+          if (this.active === 0) {
+            //step 1
+            //parse json
+
+          }
+          if (this.active++ > 2) this.active = 0;
         },
         onSubmit() {
             console.log('submit!');
@@ -69,6 +73,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.step-contain{
+  margin: 10px 20px 0px 20px;
+}
+
+.step1 {
+  .json-input{
+    >textarea{
+      height: 350px;
+    }
+  }
+}
 
 </style>
